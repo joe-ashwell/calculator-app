001f01fbf2425517121d7a5bf8a71a6f538e032d
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-06-28 20:02:22 +0100

    add querySelector to buttons

--- calculator-app.js
+++ calculator-app.js
@@ -1,1 +1,26 @@
+// To target the display screen
 const input = document.querySelector('p');
+
+// To target the operation buttons
+const ac = document.querySelector('.btn-ac');
+const c = document.querySelector('.bnt-c');
+const posNeg = document.querySelector('.btn-pos-neg');
+const div = document.querySelector('.btn-div');
+const multiply = document.querySelector('.btn-multiply');
+const minus = document.querySelector('.btn-minus');
+const plus = document.querySelector('.btn-plus')
+const decimal = document.querySelector('.btn-decimal');
+const equals = document.querySelector('.btn-equals')
+
+// To target the number buttons
+const zero = document.querySelector('.btn-zero')
+const one = document.querySelector('.btn-one');
+const two = document.querySelector('.btn-two')
+const three = document.querySelector('.btn-three');
+const four = document.querySelector('.btn-four');
+const five = document.querySelector('.btn-five');
+const six = document.querySelector('.btn-six');
+const seven = document.querySelector('.btn-seven');
+const eight = document.querySelector('.btn-eight');
+const nine = document.querySelector('.btn-nine');
+
